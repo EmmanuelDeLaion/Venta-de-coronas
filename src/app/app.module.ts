5cc6ab0eb886c4a4a9473dc262c63d341194d6b4
ErickGonzalezISC <gonzlez.lopez.16186@itsmante.edu.mx>
2020-10-30 08:17:34 -0700

    Login y navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,23 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module'
 import { from } from 'rxjs';
 
+import {APP_ROUTING} from './rutas';
+
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
